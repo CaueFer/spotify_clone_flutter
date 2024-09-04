03b0b7ba32c1850772eaf17a9c7e754035ca8551
CaueFer <cauefernandescaetano@gmail.com>
2024-09-04 12:34:56 -0300

    first commit

---
+++ lib/core/configs/theme/app_colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+
+  static const primary = Color(0xff42C83C);
+
+  static const lightBackground = Color(0xffF2F2F2);
+  static const darkBackground = Color(0xff070707);
+
+  static const textSecondary = Color(0xff616161);
+
+  static const textPrimaryLight = Color(0xffDBDBDB);
+  static const textPrimaryDark = Color(0xff000000);
+}
